522651df838205029cb6616be0982fed1bba4570
Harshit Singh <harshit@vinsol.com>
2020-03-23 21:34:17 +0530

    Commit with final changes

--- Pangram.kt
+++ Pangram.kt
@@ -5,7 +5,7 @@ Method that takes a String sentence to check if the sentence is a Pangram or not
 fun isPangram(sentence: String): Boolean{
     val alphaSet= HashSet<Char>()
     for(c in sentence){
-        if(c != ' '){
+        if(c.isLetter()){
             alphaSet.add(c.toLowerCase())
         }
     }
@@ -19,7 +19,7 @@ fun isPangram(sentence: String): Boolean{
 Method main to check the working of method isPangram() with a correct and an incorrect sentence
  */
 fun main(args: Array<String>){
-    val correctSentence="the quick brown fox jumps over a lazy dog"
+    val correctSentence="the quick brown fox jumps over a lazy dog:12:;sa kasl"
     val incorrectSentence="the quik brown fox jumps over a lay dog"
     println(isPangram(correctSentence))
     println(isPangram(incorrectSentence))
